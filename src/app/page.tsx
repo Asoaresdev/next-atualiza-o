eca2761c23ce225cf24e3ab32669ce8b2a6d08d5
Andrea Soares <asoaresdev@gmail.com>
2024-03-15 15:53:24 -0300

    feat: routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Acesso from "@/components/acesso";
+import ServerFetch from "@/components/serverFetch";
 
 export default function HomePage() {
 
@@ -6,6 +7,7 @@ export default function HomePage() {
     <main >
       <h1>Home</h1>
       <Acesso />
+      <ServerFetch />
     </main>
   );
 }
