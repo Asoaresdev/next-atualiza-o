e9dcff08058d641f4c0cee751a079bb7cd3e0f28
Andrea Soares <asoaresdev@gmail.com>
2024-03-15 10:32:35 -0300

    feat: server and client component

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,3 +1,4 @@
+import Width from "@/components/width"
 import { Metadata } from "next"
 
 // meta tags, descrição da pagina 
@@ -9,7 +10,7 @@ export default function SobrePage() {
     return(
         <main>
             <h2>Sobre</h2>
-            <h2 id="empresa"></h2>
+            <Width />   
         </main>
     )
 }
